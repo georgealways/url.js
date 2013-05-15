32b3ba3f94a9b411390a19e1d51b4402ddb689c0
George Michael Brower <georgebrower@google.com>
2013-05-15 14:25:40 -0400

    grunt

--- url.js
+++ url.js
@@ -1,1 +1,32 @@
-(function(e){var a={},f=e.indexOf("#");e.substring(e.indexOf("?")).replace(/([^?=&]+)(=([^&]+))?/g,function(c,d,b,e){a[d]=e});a["boolean"]=function(c,d){return!a.hasOwnProperty(c)?d:"false"!==a[c]};a["float"]=function(c,d){var b=parseFloat(a[c]);return b!=b?d:b};a["int"]=function(c,d){var b=parseInt(a[c],10);return b!=b?d:b};a.hash=-1==f?void 0:e.substring(f+1);a.setUrl=arguments.callee;window.url=a})(location.href);
+(function(url) {
+
+  var result = {},
+      hashLoc = url.indexOf('#');
+
+  url.substring(url.indexOf('?')).replace(
+    /([^?=&]+)(=([^&]+))?/g,
+    function($0, $1, $2, $3) {
+      result[$1] = decodeURIComponent($3);
+    }
+  );
+
+  result['bool'] = function(name, defaultValue) {
+    if (!result.hasOwnProperty(name))
+      return defaultValue;
+    return result[name] !== 'false';
+  };
+
+  result['num'] = function(name, defaultValue) {
+    var r = parseFloat(result[name]);
+    if (r != r) 
+      return defaultValue;
+    return r;
+  };
+
+  result['hash'] = hashLoc == -1 ? undefined : url.substring(hashLoc+1);
+
+  result['setUrl'] = arguments.callee; 
+
+  window['url'] = result;
+
+})(window.location.href);
